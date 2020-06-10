451074d91d6bd24afcc672449755e968772b7ef2
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-06-10 11:21:21 -0400

    .

---
+++ SimulacionBasketball/SimulacionBasketball/SimulacionBasketball.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace SimulacionBasketball
+{
+    partial class SimulacionBasketball
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imagenDado = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.turno1 = new System.Windows.Forms.Label();
+            this.turno2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encestolabel = new System.Windows.Forms.Label();
+            this.fallolabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fallo2label = new System.Windows.Forms.Label();
+            this.encesto2label = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenDado)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.imagenDado);
+            this.groupBox1.Location = new System.Drawing.Point(158, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 386);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // imagenDado
+            // 
+            this.imagenDado.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.imagenDado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imagenDado.Image = global::SimulacionBasketball.Properties.Resources._187441;
+            this.imagenDado.Location = new System.Drawing.Point(3, 16);
+            this.imagenDado.Name = "imagenDado";
+            this.imagenDado.Size = new System.Drawing.Size(599, 367);
+            this.imagenDado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagenDado.TabIndex = 0;
+            this.imagenDado.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 392);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(133, 135);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Jugado Diestro";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::SimulacionBasketball.Properties.Resources.clippers;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox3.Location = new System.Drawing.Point(779, 392);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(133, 135);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Jugador Zurdo";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = global::SimulacionBasketball.Properties.Resources.FlickeringCleverGraysquirrel_size_restricted;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // turno1
+            // 
+            this.turno1.AutoSize = true;
+            this.turno1.Font = new System.Drawing.Font("Lucida Sans", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.turno1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.turno1.Location = new System.Drawing.Point(15, 349);
+            this.turno1.Name = "turno1";
+            this.turno1.Size = new System.Drawing.Size(47, 15);
+            this.turno1.TabIndex = 17;
+            this.turno1.Text = "Turno";
+            this.turno1.Visible = false;
+            // 
+            // turno2
+            // 
+            this.turno2.AutoSize = true;
+            this.turno2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turno2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.turno2.Location = new System.Drawing.Point(789, 349);
+            this.turno2.Name = "turno2";
+            this.turno2.Size = new System.Drawing.Size(47, 15);
+            this.turno2.TabIndex = 18;
+            this.turno2.Text = "Turno";
+            this.turno2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Image = global::SimulacionBasketball.Properties.Resources.basketball256_24892;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(34, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 19;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(13, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 32);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Turno del Jugador";
+            // 
+            // encestolabel
+            // 
+            this.encestolabel.AutoSize = true;
+            this.encestolabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.encestolabel.Location = new System.Drawing.Point(230, 453);
+            this.encestolabel.Name = "encestolabel";
+            this.encestolabel.Size = new System.Drawing.Size(13, 13);
+            this.encestolabel.TabIndex = 21;
+            this.encestolabel.Text = "0";
+            // 
+            // fallolabel
+            // 
+            this.fallolabel.AutoSize = true;
+            this.fallolabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.fallolabel.Location = new System.Drawing.Point(230, 488);
+            this.fallolabel.Name = "fallolabel";
+            this.fallolabel.Size = new System.Drawing.Size(13, 13);
+            this.fallolabel.TabIndex = 22;
+            this.fallolabel.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(175, 453);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Encesto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(175, 488);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Fallo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(670, 488);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Fallo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(670, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Encesto:";
+            // 
+            // fallo2label
+            // 
+            this.fallo2label.AutoSize = true;
+            this.fallo2label.ForeColor = System.Drawing.SystemColors.Control;
+            this.fallo2label.Location = new System.Drawing.Point(725, 488);
+            this.fallo2label.Name = "fallo2label";
+            this.fallo2label.Size = new System.Drawing.Size(13, 13);
+            this.fallo2label.TabIndex = 26;
+            this.fallo2label.Text = "0";
+            // 
+            // encesto2label
+            // 
+            this.encesto2label.AutoSize = true;
+            this.encesto2label.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.encesto2label.Location = new System.Drawing.Point(725, 453);
+            this.encesto2label.Name = "encesto2label";
+            this.encesto2label.Size = new System.Drawing.Size(13, 13);
+            this.encesto2label.TabIndex = 25;
+            this.encesto2label.Text = "0";
+            // 
+            // SimulacionBasketball
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(954, 550);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fallo2label);
+            this.Controls.Add(this.encesto2label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fallolabel);
+            this.Controls.Add(this.encestolabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.turno2);
+            this.Controls.Add(this.turno1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SimulacionBasketball";
+            this.Text = "Simulacion Basketball";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imagenDado)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox imagenDado;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label turno1;
+        private System.Windows.Forms.Label turno2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label encestolabel;
+        private System.Windows.Forms.Label fallolabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label fallo2label;
+        private System.Windows.Forms.Label encesto2label;
+    }
+}
+
---
+++ SimulacionBasketball/SimulacionBasketball/SimulacionBasketball.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionBasketball
+{
+    public partial class SimulacionBasketball : Form
+    {
+        int Jugador1 = 0, Jugador2 = 0;
+        int encesto = 0, fallo = 0;
+        int encesto2 = 0, fallo2 = 0;
+
+        public SimulacionBasketball()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            turno();
+        }
+
+        public void turno()
+        {
+            Random rand = new Random();
+
+            int num;
+            num = rand.Next(1, 101);
+
+            if (num > 87) //Jugador 2
+            {
+                turno2.Visible = true;
+                turno1.Visible = false;
+                Jugador2 = 2;
+                encesto = 0; fallo = 0;
+                encestolabel.Text = "0";
+                fallolabel.Text = "0";
+                Posicion2();
+            }
+            else //Jugador 1
+            {
+                Jugador1 = 1;
+                turno1.Visible = true;
+                turno2.Visible = false;
+                encesto2 = 0; fallo2 = 0;
+                encesto2label.Text = "0";
+                fallo2label.Text = "0";
+                Posicion1();
+            }
+        }
+
+        public void Posicion1()
+        {
+
+            int IZ2 = 1, IZ1 = 2, CE = 3, DE2 = 4, DE = 5, CO = 6;
+            if (Jugador1 == 1) //Jugador diestro Jugador 1
+            {
+                Random rando = new Random();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    Random rand = new Random();
+                    decimal posicion = rand.Next(1, 7);
+                    if (posicion == IZ2)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                    if (posicion == IZ1)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                    if (posicion == CE)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                    if (posicion == DE)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+                       if (probabilidad > 35)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                    if (posicion == DE2)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+                        if (probabilidad > 35)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                    if (posicion == CO)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+                                   if (probabilidad > 85)
+                        {
+                            encesto++;
+                            encestolabel.Text = encesto.ToString();
+                        }
+                        else
+                        {
+                            fallo++;
+                            fallolabel.Text = fallo.ToString();
+                        }
+                    }
+                }
+                encesto = 0; fallo = 0;
+            }
+        }
+
+        public void Posicion2()
+        {
+            int IZ22 = 1, IZ11 = 2, CE1 = 3, DE22 = 4, DE1 = 5, CO1 = 6;
+
+            if (Jugador2 == 2) //jugador 2
+            {
+
+                Random rando = new Random();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    Random rand = new Random();
+                    decimal posicion = rand.Next(1, 7);
+                    if (posicion == IZ22)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 25)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                    if (posicion == IZ11)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 25)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                    if (posicion == CE1)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                    if (posicion == DE1)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                    if (posicion == DE22)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                    if (posicion == CO1)
+                    {
+                        int probabilidad = rando.Next(1, 101);
+
+                        if (probabilidad > 85)
+                        {
+                            encesto2++;
+                            encesto2label.Text = encesto2.ToString();
+                        }
+                        else
+                        {
+                            fallo2++;
+                            fallo2label.Text = fallo2.ToString();
+                        }
+                    }
+                }
+                encesto2 = 0; fallo2 = 0;
+            }
+        }
+
+    }
+}
+
